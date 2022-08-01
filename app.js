0ccb4267107d405d2c1aa95997df3534675da68a
MrBien0405 <110025434+MrBien0405@users.noreply.github.com>
2022-08-02 00:27:51 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+//Bài 1:sử dụng vòng lặp để đếm từ 1 đến 100. Khi số là 99, hiện thị hộp thoại thông báo đã hoàn thành
+// for(i = 1; i < 100; i = i +1)  {
+//     console.log(i);
+// }
+// if( i = 99){
+//     console.log("Hoàn thành");
+// }
+// Bài 2: Hiện ra 20 số trong fibonacci đầu tiên
+// let n = Number(prompt("nhập số n vào"))
+// let a = 0;
+// let b = 1;
+// for(i = 1; i <= n; i = i + 1){
+//     console.log(a);
+//     tong = a + b;
+//     a = b
+//     b = tong
+// }
+
+// Bài 3: Tìm số đầu tiên trong dãy fibonacci chia hết cho 5
+// let a = 0
+// let b = 1
+// let state = true
+// while (state){
+//     let c = a + b
+//     a = b
+//     b = c
+//     if ( a % 5 === 0){
+//         state = false;
+//     }console.log(`${a}`)
+// }
+
+
+
+// Bài 4: Tính tổng 20 số đầu tiên trong dãy fibonacci
+
+
+
+
+
+
+
+//Bài 6:Hãy viết một chương trình in ra các số từ 1 đến 100. Nhưng nếu số chia hết cho 3 thì in ra "Fizz", 5 thì in ra "Buzz" thay vì in ra số đó. Và nếu số đó chia hết cho cả 3 và 5 thì in ra chữ "FizzBuzz".
+// for(let i = 1; i <= 100; i = i + 1)  {
+// if ( i % 3 == 0 ){
+//     console.log("Fizz");
+// }else if( i % 5 == 0){
+//     console.log("Buzz");
+// }else if(i % 3 == 0 && i % 5 == 0){
+//     console.log("Buzz");
+// }else{
+//     console.log(i);
+// }
+// }
+// 
+//Bài 7: Tính giai thừa của 1 số nguyên dương
